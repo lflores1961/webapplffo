d44d038a2a3051d1b14213a91445293a32d7addd
Fernando Flores <fernando.flores1961@gmail.com>
2021-07-09 15:13:45 -0500

    Main page with jQuery, bootstrap and Popper add through yarn/webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,17 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
+import "bootstrap"
+import "./tarjeta"
+import "packs/styles.scss"
 
+document.addEventListener("turbolinks:load",
+  () => {
+    $('[data-toggle = "tooltip"]').tooltip()
+    $('[data-toggle = "popover]').popover()
+  }
+)
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get main" do
+    get static_pages_main_url
+    assert_response :success
+  end
+end
